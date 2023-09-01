4f59f811a878f763721ee5f98e4a710fa73f1eff
Muzeffer <muzeffer9b2004@gmail.com>
2023-09-01 15:22:53 +0400

    Bank Account App ready

---
+++ src/index.test.js
@@ -0,0 +1,9 @@
+// const { BankAccount } = require('./index');
+
+// describe('Bank Account App', () => {
+//     describe('Deposit function: ', () => {
+//         test('should be defined', () => {
+//             expect('1').toBeTruthy();
+//         })
+//     })
+// })
